0472cbd5dbb850c6f2a79b7dd7eba5b1366468a9
Luca Somaschini <somas99@gmail.com>
2022-09-13 14:31:36 +0200

    Added header & basic php array

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+        $faq = [
+            [
+                'question' => 'text',
+                'answer' => 'text'
+            ],
+            [
+                'question' => 'text',
+                'answer' => 'text'
+            ],
+            [
+                'question' => 'text',
+                'answer' => 'text'
+            ],
+            [
+                'question' => 'text',
+                'answer' => 'text'
+            ],
+            [
+                'question' => 'text',
+                'answer' => 'text'
+            ],
+            [
+                'question' => 'text',
+                'answer' => 'text'
+            ]
+        ]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Google Faq</title>
+</head>
+<body>
+    <header>
+        <div class="container widthMargin d_flex jc_sb">
+            <div class="d_flex jc_sb al_m">
+                <img src="img/google.png" alt="Google">
+                <h1>Privacy e Termini</h1>
+            </div>
+            <div class="d_flex jc_sb al_m">
+                <i class="fa-solid fa-bars"></i>
+                <span id="profile">A.</span>
+            </div>
+        </div>
+        <nav class="widthMargin">
+            <ul class="d_flex">
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li>Domande frequenti</li>
+            </ul>
+        </nav>
+    </header>
+    <hr>
+    <main>
+        
+    </main>
+</body>
+</html>
